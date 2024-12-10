c0811b0c76e93e0afce99eca1a81653039b09b66
Kurtsley <73447098+Kurtsley@users.noreply.github.com>
2024-12-10 14:36:51 -0800

    Initial

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'mckenzie2-cache';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/data/addresses.geojson',
+  '/static/manifest.json',
+  '/static/map.png',
+  '/css/style.css',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/data/addresses.geojson')) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then((response) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  }
+});
